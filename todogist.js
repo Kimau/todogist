c2d29e79c18ca313c1d3156736eab01568558654
Claire Blackshaw <evilkimau@gmail.com>
2020-12-05 17:21:25 +0000

    Bleh

--- todogist.js
+++ todogist.js
@@ -1,18 +1,11 @@
 /*jshint esversion: 8 */
 
-const heartbeatInterval = 1000 * 60; //ms between PING's
-const reconnectInterval = 1000 * 3; //ms to wait before reconnect
-const badgeList = {};
-const msgLog = [];
-const logThese = ["PRIVMSG", "NOTICE", "JOIN", "PART"];
-const twitchRateBucket = {
-    ts: 0,
-    hot: 0
-};
-const wsock = {
-    chat: null,
-    event: null
-}
+const todo_lines = [];
+const carddb = {};
+let pinboard = null;
+let card_seq = 100;
+
+// Helpers
 
 function encodeUri(url, data) {
     if(typeof(data) == "string") return url + "?" + encodeURIComponent(data);
@@ -54,25 +47,113 @@ function makeTag(tag, cn, text) {
 
 function lget(name) { let v = window.localStorage.getItem(name); try { return JSON.parse(v); } catch (error) { return v; }}
 function lset(name,value) { if(typeof(value) != "string") value = JSON.stringify(value); return window.localStorage.setItem(name, value); }
+const openFile = async () => { try { 
+    const [handle] = await window. showOpenFilePicker(); 
+    const file = await handle.getFile();
+    const contents = await file.text(); 
+    return contents; 
+} catch (err) { console.error(err.name, err.message); return null; } };
 
 ///////////////////////////////////////////////////////////////////////////////
 // Application Code
 
-function doAuth() {
-    let oAuthState = btoa(Math.random());
-    lset("authstate", oAuthState)
-    window.location = encodeUri("https://github.com/login/oauth/authorize", {
-        client_id: client.id,
-        redirect_uri: "http://localhost:8000/",
-        scope: "gist",
-        state: oAuthState
-    });
+async function cardTitleInput(e) {
+    if((e.type == "keydown") && (e.code != "Enter")) return;
+    
+    e.target.innerText = e.target.innerText.trim();
+    e.target.contentEditable = false;
+    e.target.parentElement.classList.remove("new");
+    e.target.parentElement.setAttribute("ts", Number(new Date()));
+    
+    const seq = Number(e.target.parentElement.getAttribute("seq"));
+    if(seq > 0) { } else {
+        e.target.parentElement.setAttribute("seq", card_seq);
+        card_seq++;
+    }
+
+    scrapCardsForChanges();
+}
+
+async function createNewCard(e) {
+    const newCard = makeTag("div", "card new");
+    const titleOfCard = makeTag("h1", "", "");
+    newCard.appendChild(titleOfCard);
+    newCard.appendChild(makeTag("p", "", "..."));
+    newCard.style.left = e.offsetX + "px";
+    newCard.style.top = e.offsetY + "px";
+    
+    newCard.setAttribute("x", e.offsetX);
+    newCard.setAttribute("y", e.offsetY);
+    
+    pinboard.appendChild(newCard);
+
+    titleOfCard.contentEditable = "true";
+    titleOfCard.addEventListener("keydown", cardTitleInput, { passive: true });
+    titleOfCard.addEventListener("focusout", cardTitleInput, { passive: true });
+    titleOfCard.focus();
+}
+
+async function createCard(cData) {
+    const newCard = makeTag("div", "card new");
+    const titleOfCard = makeTag("h1", "", cData.title);
+    newCard.appendChild(titleOfCard);
+    newCard.appendChild(makeTag("p", "", cData.desc));
+    newCard.style.left = cData.x + "px";
+    newCard.style.top = cData.y + "px";
+    
+    newCard.setAttribute("x", cData.x);
+    newCard.setAttribute("y", cData.y);
+    newCard.setAttribute("ts", cData.ts);
+    newCard.setAttribute("seq", cData.seq);
+    
+    pinboard.appendChild(newCard);
+}
+
+async function canvasClick(e) {
+    if((e.target == pinboard) || (e.path.length <= 3))
+        createNewCard(e);
+}
+
+async function scrapCardsForChanges() {
+    const seqList = [];
+
+    const clist = document.getElementsByClassName("card");
+    for (let i = 0; i < clist.length; i++) {
+        const cData = { title: "", desc: "", seq: -1, ts: 0 };
+        const element = clist[i];
+
+        cData.title = element.getElementsByTagName("h1")[0].innerText;
+        cData.desc = element.getElementsByTagName("p")[0].innerText;
+        cData.seq = Number(element.getAttribute("seq"));
+        cData.ts = Number(element.getAttribute("ts"));
+        cData.x = Number(element.getAttribute("x"));
+        cData.y = Number(element.getAttribute("y"));
+
+        if((cData.seq > 0) && (cData.ts > 0)) {
+            carddb[cData.seq] = cData;
+            seqList.push(cData.seq);
+            lset("card_" + cData.seq, cData);
+        }
+    }
+
+    lset("seqList", seqList);
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // Window Events
 window.addEventListener("load", async () => {
+    pinboard = document.getElementById("pinboard");    
+    document.addEventListener("click", canvasClick, { passive: true });
 
+    const seqList = lget("seqList");
+    for (let i = 0; (seqList) && (i < seqList.length); i++) {
+        const cData = lget("card_" + seqList[i]);
+        if((cData.seq > 0) && (cData.ts > 0)) {
+            carddb[cData.seq] = cData;
+            createCard(cData);
 
+            if(cData.seq >= card_seq) card_seq = cData.seq + 1;
+        }
+    }
 });
